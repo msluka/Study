24a7503ca49b7537d3d50e703d09cc06894bcd68
Msluka <969179@gmail.com>
2017-04-14 23:50:49 +0200

    Update

--- C#/C#.cs
+++ C#/C#.cs
@@ -1656,3 +1656,32 @@ namespace RegExApplication
       }
    }
 }
+
+///The following example matches words that start with 'm' and ends with 'e':
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegExApplication
+{
+   class Program
+   {
+      private static void showMatch(string text, string expr)
+      {
+         Console.WriteLine("The Expression: " + expr);
+         MatchCollection mc = Regex.Matches(text, expr);
+         foreach (Match m in mc)
+         {
+            Console.WriteLine(m);
+         }
+      }
+      static void Main(string[] args)
+      {
+         string str = "make maze and manage to measure it";
+
+         Console.WriteLine("Matching words start with 'm' and ends with 'e':");
+         showMatch(str, @"\bm\S*e\b");
+         Console.ReadKey();
+      }
+   }
+}
