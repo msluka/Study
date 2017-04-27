6a3a8990e4464592c2c3db5c1f565cc63c388bef
Msluka <969179@gmail.com>
2017-04-27 15:26:16 +0200

    Update

--- C#/C#.cs
+++ C#/C#.cs
@@ -1013,6 +1013,36 @@ class Program
         }
     }
 	
+
+/// The readonly Modifier
+/// prevents a member of a class from being modified after construction. 
+/// It means that the field declared as readonly can be modified only when you 
+/// declare it or from within a constructor.
+
+	class Person 
+	{
+	  private readonly string name = "John"; 
+	  public Person(string name) {
+		this.name = name; 
+	  }
+	}
+
+// If we try to modify the name field anywhere else, we will get an error.
+
+/// There are three major differences between readonly and const fields. 
+/// - First, a constant field must be initialized when it is declared, 
+///   whereas a readonly field can be declared without initialization, as in:
+
+	  readonly string name; // OK
+	  const double PI; // Error
+	  
+/// - Second, a readonly field value can be changed in a constructor, but a constant value cannot.
+
+/// - Third, the readonly field can be assigned a value that is a result of a calculation, 
+///   but constants cannot, as in:
+
+	  readonly double a = Math.Sin(60); // OK
+	  const double b = Math.Sin(60); // Error! 
 	
 ///Length returns the length of the string.
 
