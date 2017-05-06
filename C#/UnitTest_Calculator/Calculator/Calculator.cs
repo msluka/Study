1d39fe878866bae4c965f44b8206de5b7d07ee26
Msluka <969179@gmail.com>
2017-05-06 22:26:02 +0200

    UnitTest_Calculator

---
+++ C#/UnitTest_Calculator/Calculator/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+            //throw new NotImplementedException();
+        }
+
+        public int Subtract(int a, int b)
+        {
+            throw new NotImplementedException();
+            
+
+        }
+
+        public int Multiplay (int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / (double) b;
+        }
+    }
+}
---
+++ C#/UnitTest_Calculator/CalculatorTest/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+
+    
+    public class CalculatorTests
+    {
+        private Calculator.Calculator _calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _calculator = new Calculator.Calculator();
+        }
+
+        [TestMethod]
+
+        public void Add_PassTwoSmallIntegers_ReturnsAdditionResult()
+        {
+            //Arrange
+            var a = 10;
+            var b = 4;
+            var expectedResult = 14;
+
+            //Act
+            var result = _calculator.Add(a, b);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Add_PassTwoMaxlIntegers_ThrowsOverflowException()
+        {
+
+            //Arrange
+            var a = int.MaxValue;
+            var b = int.MinValue;
+
+            //Act
+            var result = _calculator.Add(a, b);
+
+            //Assert
+        }
+
+        [TestMethod]
+
+        public void Devide_PassTwoIntegersWhichAreDevide_ReturnsCorrectResult()
+        {
+            //Arrange
+            var a = 10;
+            var b = 5;
+            var expectedResult = 2d;
+
+            //Act
+            var result = _calculator.Divide(a, b);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Devide_PassTwoIntegersWhichAreNotDevided_ThrowsOverflowException()
+        {
+            //Arrange
+            var a = 12;
+            var b = 0;
+           
+            //Act
+            var result = _calculator.Divide(a, b);
+
+            //Assert
+        }
+    }
+}
