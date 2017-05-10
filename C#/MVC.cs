e31c5ee5be134d0b11a7ce7948c7df59d32dbfbb
Msluka <969179@gmail.com>
2017-05-10 22:56:10 +0200

    VeiwData and ViewBag

---
+++ C#/MVC.cs
@@ -0,0 +1,21 @@
+/// ViewData
+
+//  In controller ActionResult
+	
+	ViewData["key1"]="some data";
+
+	
+//	In View
+    
+	@ViewData["key1"]
+	
+	
+/// ViewBag
+
+//	In controller ActionResult           
+	
+	ViewBag.key1="some data";
+
+//	In View
+   
+	@ViewBag.key1
