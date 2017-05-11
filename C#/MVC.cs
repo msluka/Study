0e6e5a85de74c04c9a0c3bbb4b9ad145e44174ff
Msluka <969179@gmail.com>
2017-05-11 23:53:32 +0200

    Html Helper class and using

--- C#/MVC.cs
+++ C#/MVC.cs
@@ -118,3 +118,91 @@
 		}
 
 	</div>
+	
+
+	
+/// Example with loosly typed TextBox:
+	
+//  Models
+
+
+	namespace InputScreenMVC.Models
+	{
+		public class Customer
+		{
+			public int Id { get; set; }
+			public string CustomerCode { get; set; }
+			public double Amount { get; set; }
+		}
+	}
+	
+// Views / FillCustomer
+
+
+	@model InputScreenMVC.Models.Customer
+
+
+	<body>
+
+		@using (Html.BeginForm("DisplayCustomer", "Customer", FormMethod.Post))
+		{
+			
+			@Html.Label("CustomerId", "Enter Customer Id")
+			@Html.TextBox("CustomerId", Model, new { @class = "form-control" })
+			<br />
+			@Html.Label("CustomerCode", "Enter Customer Code")
+			@Html.TextBox("CustomerCode", Model, new { @class = "form-control" })
+			<br />
+			@Html.Label("Amount", "Enter Customer Amount")
+			@Html.TextBox("Amount", Model, new { @class = "form-control" })
+			<br />
+			
+			<input type="submit" class="btn btn-info" value="Click to Submit">
+
+		}
+
+	</body>
+
+
+// Views / DisplayCustomer
+	
+
+	@model InputScreenMVC.Models.Customer
+	
+	<div>
+		
+		Customer Id is: @Model.Id<br/>
+		Customer Code is: @Model.CustomerCode <br/>
+		@if (Model.Amount > 100)
+		{
+			<span>This is VIP Customer and Amount is:</span>
+			<span>@Model.Amount</span>
+		}
+		else
+		{
+			<span>This is usual Customer and Amount is:</span>
+			<span>@Model.Amount</span>
+		}
+
+	</div>
+	
+	<div>
+		@Html.ActionLink("Back", "FillCustomer", "Customer", new {@class="btn btn-primary btn-large"})
+	</div>
+
+
+// Controller / CustomerController
+
+		
+		[HttpPost]
+        public ActionResult DisplayCustomer(Customer obj)
+        {
+            return View(obj);
+        }
+		
+// Summary
+
+//	1. Html Helper class creates Html elements.
+//	2. Submit button Calls DisplayCustomer Action in CustomController.
+//	3. DisplayCustomer Action takes as a parameter Customer object that 
+//	   was created using Html Helper Class and pass it to the View.
