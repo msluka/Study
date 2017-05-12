55776c9e0d4137ab1f263cc28bfc3fe6b75a1974
Msluka <969179@gmail.com>
2017-05-13 01:52:06 +0200

    MVC-Student

---
+++ C#/MVC/MVC/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Student
+    {
+        [Display(Name = "Id")]
+        public int StudentId { get; set; }
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
