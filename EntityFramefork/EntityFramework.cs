ee141e6fbf171657fedfaa85cf987b3db6027e51
Msluka <969179@gmail.com>
2017-08-07 15:40:38 +0200

    CodeFirst

--- EntityFramefork/EntityFramework.cs
+++ EntityFramefork/EntityFramework.cs
@@ -157,4 +157,164 @@ Entity Framework
 		
 		   You will see in your solution explorer that 
 				ModelFirstDemoModel.Context.tt and ModelFirstDemoModel.tt 
-		   templates are generated.
+		   templates are generated. */
+		   
+		   
+/*
+	Code First
+	-----------
+		https://www.tutorialspoint.com/entity_framework/entity_framework_first_example.htm
+		
+		https://blog.rajsoftware.com/2014/07/17/entity-framework-updating-code-first-existing-entity-using-foreign-key-ids-mistakes/
+*/		
+		
+		using System;
+		using System.Collections.Generic;
+		using System.ComponentModel.DataAnnotations.Schema;
+		using System.Data.Entity;
+		using System.Linq;
+
+
+		namespace CodeFirstApp
+		{
+			public class MyContext : DbContext
+			{
+				public MyContext() : base("MyDb") { }
+
+				public virtual DbSet<Employee> Employees { get; set; }
+				public virtual DbSet<Department> Departments { get; set; }
+
+
+			}
+
+			public class Department
+			{
+				public int ID { get; set; }
+				public string Name { get; set; }
+				public string Location { get; set; }
+
+				public virtual ICollection<Employee> Employees { get; set; }
+
+			}
+
+			public class Employee
+			{
+				public int Id { get; set; }
+				[Column("Name")]
+				public string Firstname { get; set; }
+				public string Lastname { get; set; }
+				public int Salary { get; set; }
+				
+				public virtual Department Department { get; set; }
+			}
+
+
+			class Program
+			{
+				static void Main(string[] args)
+				{
+					using (var db = new MyContext())
+					{
+
+						//Change an employee name
+						var employee = (from d in db.Employees where d.Firstname == "David" select d).Single();
+						//var employee = db.Employees.Single(x => x.Firstname == "David");
+
+						if (employee.Department != null)
+						{
+							Console.WriteLine("Before: " + employee.Firstname + ' ' + employee.Department.Location);
+						}
+						else
+						{
+							Console.WriteLine("Before: " + employee.Firstname + " Location Unknown");
+
+						}
+
+						var department = (from d in db.Departments where d.Location == "London" select d).Single();
+
+						//Foreign Key
+						employee.Department = department;
+						db.SaveChanges();
+
+
+						Console.WriteLine("After: " + employee.Firstname + ' ' + employee.Department.Location);
+						Console.WriteLine("--------------------");
+
+						//---------------------------------------------------------------------------------------
+
+						// Eager Loading
+
+						var dep = db.Departments.Include(x => x.Employees).ToList();
+
+						foreach (var e in dep)
+						{
+							Console.WriteLine(e.Name + ' ' + e.Location);
+
+							foreach (var b in e.Employees)
+							{
+								Console.WriteLine(b.Firstname + ' ' + b.Lastname + ' ' + b.Salary);
+							}
+
+							Console.WriteLine();
+						}
+
+						Console.WriteLine("--------------------");
+
+						//---------------------------------------------------------------------------------------
+
+						// Lazy Loading
+						// db.Configuration.LazyLoadingEnabled = false; // Disables Lazy Loading 
+						// or just remove virtual keyword
+
+						var dep2 = db.Departments.ToList();
+
+						foreach (var e in dep2)
+						{
+							Console.WriteLine(e.Name + ' ' + e.Location);
+
+							foreach (var b in e.Employees)
+							{
+								Console.WriteLine(b.Firstname + ' ' + b.Lastname + ' ' + b.Salary);
+							}
+
+							Console.WriteLine();
+						}
+
+						Console.WriteLine("--------------------");
+
+						//---------------------------------------------------------------------------------------
+
+						// Native SQL
+						var dep3 = db.Departments.SqlQuery("select * from Departments").ToList();
+
+						foreach (var e in dep3)
+						{
+							Console.WriteLine(e.Name + ' ' + e.Location);
+
+							foreach (var b in e.Employees)
+							{
+								Console.WriteLine(b.Firstname + ' ' + b.Lastname + ' ' + b.Salary);
+							}
+
+							Console.WriteLine();
+
+						}
+
+						Console.WriteLine("--------------------");
+
+						//---------------------------------------------------------------------------------------
+
+
+						// SqlCommand
+						Console.WriteLine("SqlCommand");
+						var id = 3;
+						var location = "Paris";
+						var sql = @"UPDATE Departments SET Location = {0} where ID = {1}";
+						db.Database.ExecuteSqlCommand(sql, location, id);
+						
+
+
+					}
+				}
+			}
+		}
