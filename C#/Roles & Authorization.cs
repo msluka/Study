a54874da684fd4e5a447e5784984c1620753424c
Msluka <969179@gmail.com>
2017-09-10 23:44:38 +0200

    Redirect to different pages after login, based on user role

--- C#/Roles & Authorization.cs
+++ C#/Roles & Authorization.cs
@@ -1,7 +1,7 @@
 
 /// Authorize Attribute 
 	
-    // Only Users with a Role "Admin" Can Access this Page.
+    /// Only Users with a Role "Admin" Can Access this Page.
 	
 	[Authorize(Roles = "Admin")]
 	public ActionResult About()
@@ -12,7 +12,7 @@
 	}
 	
 	
-	//Only Users with a Role "Admin" or "Manager" Can Access this Page.
+	/// Only Users with a Role "Admin" or "Manager" Can Access this Page.
 	
 	[Authorize(Roles = "Admin, Manager")]
 	public ActionResult About()
@@ -20,6 +20,46 @@
 		ViewBag.Message = "Your application description page.";
 
 		return View();
+	}
+	
+	/// Redirect to different pages after login, based on user role in ASP.NET MVC 5
+	
+	// In AccountController
+	
+    // POST: /Account/Login
+	[HttpPost]
+	[AllowAnonymous]
+	[ValidateAntiForgeryToken]
+	public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(model);
+		}
+
+		var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+		
+		// Get User Role
+		var user = await UserManager.FindAsync(model.Email, model.Password);
+		switch (result)
+		{			
+			case SignInStatus.Success:
+				if (UserManager.IsInRole(user.Id, "Admin")) // Indicate Role
+				{
+					return RedirectToAction("Index", "Home"); // Define View for direction
+				}
+				
+				return RedirectToLocal(returnUrl);
+
+			case SignInStatus.LockedOut:
+				return View("Lockout");
+			case SignInStatus.RequiresVerification:
+				return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+			case SignInStatus.Failure:
+			default:
+				ModelState.AddModelError("", "Invalid login attempt.");
+				return View(model);
+		}
 	}
 	
 	
