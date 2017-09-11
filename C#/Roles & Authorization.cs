ec167294cde8214268ae8886aa33b6b2bf86afb3
Msluka <969179@gmail.com>
2017-09-11 23:54:43 +0200

    Register User with Role

--- C#/Roles & Authorization.cs
+++ C#/Roles & Authorization.cs
@@ -62,4 +62,127 @@
 		}
 	}
 	
+	/// Create User with Role
+	/// Helping source: https://www.youtube.com/watch?v=-IbNXvyyeVQ
+	
+	// In AccountViewModels
+	
+	public class RegisterUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+		
+        public string Role { get; set; }
+    }
+	
+	//In Views > Account
+	
+	@model RolesWithMvc.Models.RegisterUserViewModel
+	@{
+		ViewBag.Title = "Register User Woth Role";
+	}
+
+	<h2>@ViewBag.Title.</h2>
+
+	@using (Html.BeginForm("RegisterUser", "Account", FormMethod.Post, new { @class = "form-horizontal", role = "form" }))
+	{
+		@Html.AntiForgeryToken()
+		<h4>Create a new account.</h4>
+		<hr />
+		@Html.ValidationSummary("", new { @class = "text-danger" })
+		<div class="form-group">
+			@Html.LabelFor(m => m.Email, new { @class = "col-md-2 control-label" })
+			<div class="col-md-10">
+				@Html.TextBoxFor(m => m.Email, new { @class = "form-control" })
+			</div>
+		</div>
+		<div class="form-group">
+			@Html.LabelFor(m => m.Password, new { @class = "col-md-2 control-label" })
+			<div class="col-md-10">
+				@Html.PasswordFor(m => m.Password, new { @class = "form-control" })
+			</div>
+		</div>
+		<div class="form-group">
+			@Html.LabelFor(m => m.ConfirmPassword, new { @class = "col-md-2 control-label" })
+			<div class="col-md-10">
+				@Html.PasswordFor(m => m.ConfirmPassword, new { @class = "form-control" })
+			</div>
+		</div>
+		<div class="form-group">
+			@Html.LabelFor(m => m.Role, new { @class = "col-md-2 control-label" })
+			<div class="col-md-10">
+				@Html.TextBoxFor(m => m.Role, new { @class = "form-control" })
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-md-offset-2 col-md-10">
+				<input type="submit" class="btn btn-default" value="Register" />
+			</div>
+		</div>
+	}
+
+	@section Scripts {
+		@Scripts.Render("~/bundles/jqueryval")
+	}
+
+	
+	//In AccountController
+	
+	public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/RegisterUser
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> RegisterUser(RegisterUserViewModel model)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                    var result = await UserManager.CreateAsync(user, model.Password);
+                    
+                    if (result.Succeeded)
+                    {
+                        var roleStore = new RoleStore<IdentityRole>(context);
+                        var roleManager = new RoleManager<IdentityRole>(roleStore);
+                        await roleManager.CreateAsync(new IdentityRole(model.Role));
+                        await UserManager.AddToRoleAsync(user.Id, model.Role);
+
+
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                        
+                        return RedirectToAction("Index", "Home");
+                    }
+                    AddErrors(result);
+                }
+                
+                return View(model);
+            }
+        }
+		
+		// In _loginPartial
+		
+		<li>@Html.ActionLink("RegisterUser", "RegisterUser", "Account", routeValues: null, htmlAttributes: new { id = "registerLink" })</li>
+	
+		
+	
 	
