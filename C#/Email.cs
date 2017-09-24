a0fe54aab65f7124ebe43b91ef922201ac19c00a
Msluka <969179@gmail.com>
2017-09-24 14:52:07 +0200

    Sending Email with Attachment

--- C#/Email.cs
+++ C#/Email.cs
@@ -136,3 +136,52 @@
 
 	}
 	
+	/// Sending Email with Attachment
+	
+	/// Helpful source: https://msdn.microsoft.com/ru-ru/library/system.net.mail.mailmessage.attachments(v=vs.110).aspx
+	
+	// The same as above and additionally:
+	
+	public void SendEmail(string toEmail, string subject, string emailBody)
+	{
+		try
+		{
+			string senderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+			string senderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPass"].ToString();
+
+			SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+			client.EnableSsl = true;
+			client.Timeout = 100000;
+			client.DeliveryMethod = SmtpDeliveryMethod.Network;
+			client.UseDefaultCredentials = false;
+			client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+			MailMessage msg = new MailMessage(senderEmail, toEmail, subject, emailBody);
+			msg.IsBodyHtml = true;
+			msg.BodyEncoding = Encoding.UTF8;
+
+			msg.CC.Add("some1@gmail.com");
+			msg.Bcc.Add("some2.info@gmail.com");
+
+			string file = @"D:\welcome.jpg";
+			//string file = @"D:\welcome.pdf";
+			//string file = @"D:\welcome.xlsx";
+			//string file = @"D:\welcome.docx";
+			//string file = @"D:\welcome.mp4";
+
+			file =  @"D:\welcome.jpg"             
+			
+			Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
+			msg.Attachments.Add(data);                
+
+			client.Send(msg);
+
+		}
+		catch (Exception ex)
+		{
+			Response.Write("<script>alert('Something went wrong ! ')</script>");
+			Response.Write("The error :" + ex);
+		}
+
+	}
+	
